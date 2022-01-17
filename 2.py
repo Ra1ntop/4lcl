80fcad8a57e51a70c3b6f36260f8438a667030f4
Ra1ntop <man111play@gmail.com>
2022-01-17 17:39:36 +0200

    first commit

---
+++ 2.py
@@ -0,0 +1,14 @@
+#while True:
+#   k = int(input("Vv"))
+#    if k<=15: break
+#p = 1
+#i = 1
+#while True:
+#    p=*i
+ #   i+=1
+  #  if
+print("y=x**2")
+print("x=1,2,3, ... , 10")
+for i in range(1,11):
+    y=i**2  
+    print(f"{x} --> {y}")
